4be8905c45be3f7d732acaab0f996a95fe759695
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-03-27 17:50:10 +0900

    push SJF

--- non-preemptive/SJF.java
+++ non-preemptive/SJF.java
@@ -5,14 +5,44 @@ public class Main {
     static double[] scheduling(int n,int process[][]) {
         double matrix[]=new double[3];
 
+        //스케줄링 알고리즘을 구현
+        //결과에는 평균 대기시간, 평균 응답시간, 평균 반환시간이 포함
+
         int currentTime=-1;
         int totalWait=0;
         int totalTurnaround=0;
         int waitTime[]=new int[n];
         int turnaroundTime[]=new int[n];
 
-        //스케줄링 알고리즘을 구현
-        //결과에는 평균 대기시간, 평균 응답시간, 평균 반환시간이 포함
+        PriorityQueue<int[]>q=new PriorityQueue<>(Comparator.comparingInt(a->a[1]));
+        for(int i=0;i<n;i++){
+            if(currentTime>=process[i][0]){
+                q.add(new int[]{process[i][0],process[i][1]});
+            }
+            else{
+                if(q.isEmpty()){
+                    waitTime[i]=process[i][0]-currentTime;
+                    currentTime=process[i][1];
+                    turnaroundTime[i]=currentTime-process[i][0];
+                }
+                else{
+                    while(!q.isEmpty()){
+                        int curProcess[]=q.poll();
+                        waitTime[i]=curProcess[0]-currentTime;
+                        currentTime=curProcess[1];
+                        turnaroundTime[i]=currentTime-curProcess[0];
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            totalWait+=waitTime[i];
+            totalTurnaround+=turnaroundTime[i];
+        }
+        matrix[0]=(double)totalWait/n;
+        matrix[1]=matrix[0];
+        matrix[2]=(double)totalTurnaround/n;
 
         return matrix;
     }
@@ -20,11 +50,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        System.out.print("Process 갯수 입력: ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int process[][]=new int[n][2];
         for(int i=0;i<n;i++){
+            System.out.print("Process 도착시간과 작업시간 입력: ");
             StringTokenizer stt=new StringTokenizer(br.readLine()); // 도착시간과 작업시간을 입력
             process[i][0]=Integer.parseInt(stt.nextToken());
             process[i][1]=Integer.parseInt(stt.nextToken());
