451ba2ab951d52ae327150a053cee99cf414d4e1
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-03-27 17:46:01 +0900

    기본 변수 추가

--- preemptive/MultilevelQueue.java
+++ preemptive/MultilevelQueue.java
@@ -2,8 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] scheduling(int n,int process[][]) {
-        int matrix[]=new int[3];
+    static double[] scheduling(int n,int process[][]) {
+        double matrix[]=new double[3];
+
+        int currentTime=-1;
+        int totalWait=0;
+        int totalTurnaround=0;
+        int waitTime[]=new int[n];
+        int turnaroundTime[]=new int[n];
 
         //스케줄링 알고리즘을 구현
         //결과에는 평균 대기시간, 평균 응답시간, 평균 반환시간이 포함
@@ -24,7 +30,7 @@ public class Main {
             process[i][1]=Integer.parseInt(stt.nextToken());
         }
 
-        int result[]=scheduling(n,process);
+        double result[]=scheduling(n,process);
         bw.write("평균 대기시간 : "+result[0]+"\n");
         bw.write("평균 응답시간 : "+result[1]+"\n");
         bw.write("평균 반환시간 : "+result[2]);
