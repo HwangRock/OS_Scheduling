c7968d78aeb49ea6c133ca88009a31d654a3eb45
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-05-31 17:36:18 +0900

    feat: 선점형 우선순위 스케줄링 구현

--- src/scheduling_algorithm/preemptive/PriorityScheduling.java
+++ src/scheduling_algorithm/preemptive/PriorityScheduling.java
@@ -2,29 +2,78 @@ package scheduling_algorithm.preemptive;
 
 import scheduling_algorithm.ScheduleStrategy;
 
+import java.util.*;
+
 public class PriorityScheduling implements ScheduleStrategy {
 
-    String name="PriorityScheduling";
+    String name = "PriorityScheduling";
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public double[] scheduling(int n, int[][] process){
-        double matrix[]=new double[3];
+    public double[] scheduling(int n, int[][] process) {
+        double[] matrix = new double[3];
 
-        //스케줄링 알고리즘을 구현
-        //결과에는 평균 대기시간, 평균 응답시간, 평균 반환시간이 포함
+        int[] remainingTime = new int[n];
+        int[] startTime = new int[n];
+        boolean[] started = new boolean[n];
+        boolean[] completed = new boolean[n];
 
-        int currentTime=-1;
-        int totalWait=0;
-        int totalTurnaround=0;
-        int waitTime[]=new int[n];
-        int turnaroundTime[]=new int[n];
+        for (int i = 0; i < n; i++) {
+            remainingTime[i] = process[i][1];
+            startTime[i] = -1;
+        }
 
-        matrix[0]=(double)totalWait/n;
-        matrix[1]=matrix[0];
-        matrix[2]=(double)totalTurnaround/n;
+        int time = 0;
+        int completedCount = 0;
+        int lastProcess = -1;
+        int totalWaitingTime = 0;
+        int totalTurnaroundTime = 0;
+        int totalResponseTime = 0;
+
+        while (completedCount < n) {
+            int current = -1;
+            int minPriority = Integer.MAX_VALUE;
+
+            for (int i = 0; i < n; i++) {
+                if (process[i][0] <= time && !completed[i]) {
+                    if (process[i][2] < minPriority || (process[i][2] == minPriority && remainingTime[i] < remainingTime[current])) {
+                        minPriority = process[i][2];
+                        current = i;
+                    }
+                }
+            }
+
+            if (current == -1) {
+                time++;
+                continue;
+            }
+
+            if (!started[current]) {
+                startTime[current] = time;
+                started[current] = true;
+                totalResponseTime += time - process[current][0];
+            }
+
+            remainingTime[current]--;
+            time++;
+
+            if (remainingTime[current] == 0) {
+                completed[current] = true;
+                completedCount++;
+
+                int turnaround = time - process[current][0];
+                int waiting = turnaround - process[current][1];
+
+                totalTurnaroundTime += turnaround;
+                totalWaitingTime += waiting;
+            }
+        }
+
+        matrix[0] = (double) totalWaitingTime / n;
+        matrix[1] = (double) totalResponseTime / n;
+        matrix[2] = (double) totalTurnaroundTime / n;
 
         return matrix;
     }
